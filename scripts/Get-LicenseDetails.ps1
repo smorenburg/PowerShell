ce62f1c236d317972fba37c509b86c688e0def2c
Robin Smorenburg <66138784+smorenburg@users.noreply.github.com>
2020-07-27 14:56:49 +0200

    Add license details script

---
+++ scripts/Get-LicenseDetails.ps1
@@ -0,0 +1,63 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER InputFile 
+
+.PARAMETER OutputFile
+
+.EXAMPLE
+
+.LINK
+    https://www.smorenburg.io
+
+.NOTES
+    Author: Robin Smorenburg
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(
+        Mandatory = $true
+    )]
+    [Object]$InputFile,
+
+    [Parameter(
+    )]
+    [object]$OutputFile = ".\License Details $(Get-Date -Format yyyyMMdd-HHmm).csv"
+)
+
+$rules = Import-Csv $InputFile -Delimiter ","
+$output = New-Object System.Collections.ArrayList
+
+Connect-MSolService
+
+foreach ($rule in $rules) {
+    $user = Get-MsolUser -UserPrincipalName $rule.UserPrincipalName
+
+    if ($user.Licenses.AccountSkuId) {
+        $user.Licenses.AccountSkuId | ForEach-Object {
+            $license = $_
+
+            $licenseDetails = [ordered]@{
+                "UserPrincipalName" = [string]$rule.UserPrincipalName
+                "Present"           = "TRUE"
+                "License"           = [string]$license
+            }
+
+            [void]$output.Add((New-Object PSObject -Property $licenseDetails))
+        }     
+    }
+    else {
+        $licenseDetails = [ordered]@{
+            "UserPrincipalName" = [string]$rule.UserPrincipalName
+            "Present"           = "FALSE"
+            "License"           = $null
+        }
+
+        [void]$output.Add((New-Object PSObject -Property $licenseDetails))
+    }
+}
+
+$output | Export-Csv -Path $OutputFile -Delimiter ";" -NoTypeInformation -Encoding UTF8
